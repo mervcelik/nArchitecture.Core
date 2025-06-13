41ce838f317e00eac358662ce4e63cf588e53fc8
celik <celik@ADEM>
2025-06-13 15:33:18 +0300

    Add project files.

---
+++ Core.Persistence/Repositories/Entity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Persistence.Repositories;
+
+public class Entity<TId>
+{
+    public TId Id { get; set; }
+    public DateTime CreateDate { get; set; }
+    public DateTime? UpdateDate { get; set; }
+    public DateTime? DeleteDate { get; set; }
+
+    //constructor farklı kullanımlar için oluşturuyoruz.
+
+    public Entity()
+    {
+        Id = default;
+    }
+
+    public Entity(TId id)
+    {
+        Id = id;
+    }
+
+}
