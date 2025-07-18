f7754c60a59ff624d5572c9c1b7ec5b905e879e3
mervcelik <mrv.ozturrk@gmail.com>
2025-07-18 17:53:30 +0300

    api service eklemeleri yapıldı

--- Core.CrossCuttingConcerns/Exceptions/Handlers/HttpExceptionHandler.cs
+++ Core.CrossCuttingConcerns/Exceptions/Handlers/HttpExceptionHandler.cs
@@ -1,4 +1,5 @@
-﻿using Core.CrossCuttingConcerns.Exceptions.Extensions;
+﻿using Core.CrossCuttingConcerns.Dtos;
+using Core.CrossCuttingConcerns.Exceptions.Extensions;
 using Core.CrossCuttingConcerns.Exceptions.HttpProblemDetails;
 using Core.CrossCuttingConcerns.Exceptions.Types;
 using Microsoft.AspNetCore.Http;
@@ -6,6 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Core.CrossCuttingConcerns.Exceptions.Handlers;
@@ -18,37 +20,49 @@ public class HttpExceptionHandler : ExceptionHandler
         get => _response ?? throw new ArgumentNullException(nameof(_response));
         set => _response = value;
     }
-    protected override Task HandleException(BusinessException businessException)
+
+    private Task WriteErrorResponse(int statusCode, string errorMessage)
     {
-        Response.StatusCode = StatusCodes.Status400BadRequest;
-        string details = new BusinessProblemDetails(businessException.Message).AsJson();
-        return Response.WriteAsync(details);
+        var responseDto = ResponseDto<object>.Fail(statusCode, errorMessage);
+        Response.StatusCode = statusCode;
+        Response.ContentType = "application/json";
+        var json = JsonSerializer.Serialize(responseDto);
+        return Response.WriteAsync(json);
     }
 
-    protected override Task HandleException(Exception businessException)
+    private Task WriteErrorResponse(int statusCode, List<string> errors)
     {
-        Response.StatusCode = StatusCodes.Status400BadRequest;
-        string details = new InternalServerErrorProblemDetails().AsJson();
-        return Response.WriteAsync(details);
+        var responseDto = ResponseDto<object>.Fail(statusCode, errors);
+        Response.StatusCode = statusCode;
+        Response.ContentType = "application/json";
+        var json = JsonSerializer.Serialize(responseDto);
+        return Response.WriteAsync(json);
+    }
+
+    protected override Task HandleException(BusinessException businessException)
+    {
+        return WriteErrorResponse(StatusCodes.Status400BadRequest, businessException.Message);
+    }
+
+    protected override Task HandleException(Exception exception)
+    {
+        return WriteErrorResponse(StatusCodes.Status500InternalServerError, "Beklenmeyen bir hata oluştu.");
     }
 
     protected override Task HandleException(ValidationException validationException)
     {
-        Response.StatusCode = StatusCodes.Status400BadRequest;
-        string details = new ValidationProblemDetails(validationException.Errors).AsJson();
-        return Response.WriteAsync(details);
+        var errors = validationException.Errors.Select(e => e.Errors).ToList();
+        var x= errors.SelectMany(e => e).ToList();
+        return WriteErrorResponse(StatusCodes.Status400BadRequest, x);
     }
+
     public override Task HandleException(NotFoundException notFoundException)
     {
-        Response.StatusCode = StatusCodes.Status404NotFound;
-        string details = new NotFoundProblemDetails(notFoundException.Message).AsJson();
-        return Response.WriteAsync(details);
+        return WriteErrorResponse(StatusCodes.Status404NotFound, notFoundException.Message);
     }
 
     protected override Task HandleException(AuthorizationException authorizationException)
     {
-        Response.StatusCode = StatusCodes.Status400BadRequest;
-        string details = new AuthorizationProblemDetails(authorizationException.Message).AsJson();
-        return Response.WriteAsync(details);
+        return WriteErrorResponse(StatusCodes.Status403Forbidden, authorizationException.Message);
     }
 }
